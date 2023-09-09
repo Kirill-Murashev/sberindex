9569c8581b5c18dfc54be81f4d74e75a8e61b714
Kirill-Murashev <kirill.murashev@gmail.com>
2023-09-09 21:57:35 +0300

    August 2023 has been processed

---
+++ data_manipulation.py
@@ -0,0 +1,33 @@
+import sqlite3
+import pandas as pd
+
+# Connect to SQLite database
+conn = sqlite3.connect('sberindex.db')
+
+# List of table names to convert
+tables = [
+    'primary_deals',
+    'secondary_deals',
+    'primary_offers',
+    'secondary_offers',
+    'primary_volume',
+    'secondary_volume',
+    'exposure_period'
+]
+
+# Create an Excel writer object
+with pd.ExcelWriter('reshaped_sberindex_data.xlsx', engine='xlsxwriter') as writer:
+    # Loop through each table
+    for table in tables:
+        # Load the table into a Pandas DataFrame
+        query = f"SELECT * from {table}"
+        df = pd.read_sql_query(query, conn)
+
+        # Pivot the DataFrame
+        pivot_df = df.pivot(index='date', columns='region', values='value')
+
+        # Write DataFrame to a separate sheet in the Excel file
+        pivot_df.to_excel(writer, sheet_name=table)
+
+# Close the SQLite database connection
+conn.close()
